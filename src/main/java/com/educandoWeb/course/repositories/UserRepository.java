dcb07da25ae3c5bc541218bd15b0f53fcb093558
Vinícius Soares <viniciussou222@gmail.com>
2023-09-27 16:40:58 -0300

    service layer, component registration

--- src/main/java/com/educandoWeb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoWeb/course/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.educandoWeb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoWeb.course.entities.User;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
